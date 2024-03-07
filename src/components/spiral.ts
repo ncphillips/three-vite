6316f9514e453d78b3409f48d10167e6a4cb7830
nolan <nolan@ncphi.com>
2024-03-07 16:58:41 -0400

    extract spiral

---
+++ src/components/spiral.ts
@@ -0,0 +1,31 @@
+import {
+  Mesh,
+  SphereGeometry,
+  MeshStandardMaterial,
+  Group,
+  MathUtils,
+} from 'three'
+
+export function createSpiral() {
+  const sphere = new Mesh(
+    new SphereGeometry(0.25, 16, 16),
+    new MeshStandardMaterial({ color: 'indigo' })
+  )
+
+  sphere.position.set(0, 0, 0)
+
+  const group = new Group()
+  group.position.set(0, 3, 0)
+  group.scale.multiplyScalar(2)
+  group.rotation.y = MathUtils.degToRad(45)
+  for (let i = 0; i < 1; i += 0.001) {
+    const cloneSphere = sphere.clone()
+    cloneSphere.position.x += Math.cos(2 * Math.PI * i)
+    cloneSphere.position.y += Math.sin(2 * Math.PI * i)
+    sphere.position.z = -i * 5
+    cloneSphere.scale.multiplyScalar(0.01 + i)
+    group.add(cloneSphere)
+  }
+
+  return group
+}
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import {
   MeshStandardMaterial,
   SphereGeometry,
 } from 'three'
+import { createSpiral } from './components/spiral.ts'
 
 const container = document.querySelector<HTMLDivElement>('#scene-container')
 
@@ -50,26 +51,8 @@ pauseButton?.addEventListener('click', () => {
   world.stop()
 })
 
-const sphere = new Mesh(
-  new SphereGeometry(0.25, 16, 16),
-  new MeshStandardMaterial({ color: 'indigo' })
-)
-
-sphere.position.set(0, 0, 0)
-
-const group = new Group()
-group.position.set(0, 3, 0)
-group.scale.multiplyScalar(2)
-group.rotation.y = MathUtils.degToRad(45)
-for (let i = 0; i < 1; i += 0.001) {
-  const cloneSphere = sphere.clone()
-  cloneSphere.position.x += Math.cos(2 * Math.PI * i)
-  cloneSphere.position.y += Math.sin(2 * Math.PI * i)
-  sphere.position.z = -i * 5
-  cloneSphere.scale.multiplyScalar(0.01 + i)
-  group.add(cloneSphere)
-}
-world.add(group)
+const spiral = createSpiral()
+world.add(spiral)
 world.onTick((e) => {
-  group.rotation.z -= MathUtils.degToRad(30) * e.delta
+  spiral.rotation.z -= MathUtils.degToRad(30) * e.delta
 })
